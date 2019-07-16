af8f4fe6fd4972b02fdd7750712bdc61567137b4
Amar <amar.ravi@outlook.com>
2019-07-17 02:02:30 +0530

    First Update

---
+++ Assets/gameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    bool levelGame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelGame = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BaseBuilderCode()
+    {
+
+    }
+}
